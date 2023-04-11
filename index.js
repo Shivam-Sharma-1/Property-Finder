50d5d0781b6a6c6c27d57d939698c798b6480793
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-11 20:08:11 +0530

    add properties

--- index.js
+++ index.js
@@ -26,5 +26,46 @@ const user = {
     age: 20,
     stayedAt: ['Shimla', 'Darjeeling', 'Agra']
 };
+const properties = [
+    {
+        image: 'images/shimla-property.webp',
+        title: 'Shimla Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Shimla',
+            code: 171001,
+            country: 'India'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true
+    },
+    {
+        image: 'images/darjeeling-property.jpg',
+        title: 'Darjeeling Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Darjeeling',
+            code: 343903,
+            country: 'India'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false
+    },
+    {
+        image: 'images/agra-property.webp',
+        title: 'Agra Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'Agra',
+            code: 35433,
+            country: 'India',
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+    }
+];
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 renderUser(user.isReturning, user.firstName);
